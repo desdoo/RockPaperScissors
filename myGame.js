83d848878af72a7ed0f4258a6fae7438a96ca441
Oguzhan Soyer <soyeroguzhan@gmail.com>
2021-03-18 21:43:24 +0100

    added prompt and counter

---
+++ myGame.js
@@ -0,0 +1,56 @@
+function computerPlay(){
+    const options = ["Paper", "Scissors", "Rock"]
+    const randomValue = Math.floor(Math.random() * options.length)
+    const computerSelection =  options[randomValue]
+    return computerSelection
+}
+
+function playRound(){
+    let playerSelection = prompt("Ching Chang Chong?")
+    const computerSelection = computerPlay()
+        
+if (computerSelection===playerSelection){
+    return winner = "Both"
+     
+}
+else if (computerSelection==="Rock") {
+    if(playerSelection==="Scissors"){
+        return winner = "Computer"
+        
+    }
+        else {
+            return winner = "Player" 
+        }
+}
+else if (computerSelection==="Paper") {
+    if(playerSelection==="Rock"){
+        return winner = "Computer"
+        
+    }
+        else {
+            return winner = "Player"
+        }
+    
+}
+else if (computerSelection==="Scissors") {
+    if(playerSelection==="Paper"){
+        return winner = "Computer"
+    }
+        else {
+            return winner = "Player"
+        }
+
+}
+}
+
+function game(){
+
+let playedRound = 0
+
+for (let i=0;i<5;i++){
+    let winner = playRound()
+    window.alert(winner)
+}
+
+}
+game();
--- test.js
+++
@@ -1,3 +0,0 @@
-function test (a, b,c) {
-    console.log(a+b+c)
-}
